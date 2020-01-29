c0507401dbf84bfa925fbde4dd34d46b0cc5817e
santhu97 <38138129+santhu97@users.noreply.github.com>
2020-01-29 17:09:47 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+// Add your JavaScript code here
+console.log('It works!!'); 
+
+var  interests = {
+    "interests" :[
+        {
+            "title":"Playing Cricket",
+        },
+        {
+            "title":"Watchig Movies",
+        }
+    ]
+}
+
+interests.display = function()
+{
+    for(interest in interests.interests)
+    {
+        var santhu = HTML
+    }
+}
